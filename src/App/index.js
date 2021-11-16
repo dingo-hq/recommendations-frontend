b1e048ea48601411e7b4715128bb4f332292a71d
Eric Ong <ericong18@gmail.com>
2021-11-15 23:18:29 -0800

    Add logo to bottom of page

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ItemsList from '../components/Items/ItemsList';
+// import ItemsList from '../components/Items/ItemsList';
 import PromoRedemption from '../pages/PromoRedemption';
-import Recommendations from '../pages/Recommendations';
-import styles from './styles.module.css';
+// import Recommendations from '../pages/Recommendations';
+// import styles from './styles.module.css';
 
 function App() {
     return <PromoRedemption />;
--- src/pages/PromoRedemption/index.js
+++ src/pages/PromoRedemption/index.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import PrimaryButton from '../../components/PrimaryButton';
+import logo from '../../assets/logo.png';
 import styles from './styles.module.css';
 
 const PromoRedemption = () => {
     return (
-        <section className={styles.container}>
-            <div className={styles.info}>
-                <h1 className={styles.name}>Store Name</h1>
-                <p className={styles.description}>
-                    Present this promotional offer to the store to redeem the
-                    following
-                </p>
-            </div>
-            <div className={styles.redemption}>
-                <div className={styles.promoContainer}>
-                    <span className={styles.promoMessage}>
-                        10% off 1 Matcha Milk Tea
-                    </span>
+        <div className={styles.container}>
+            <section className={styles.content}>
+                <div className={styles.info}>
+                    <h1 className={styles.name}>Bopomofo Cafe</h1>
+                    <p className={styles.description}>
+                        Present this promotional offer to the store to redeem
+                        the following
+                    </p>
                 </div>
-                <PrimaryButton size="large">Redeem Now</PrimaryButton>
+                <div className={styles.redemption}>
+                    <div className={styles.promoContainer}>
+                        <span className={styles.promoMessage}>
+                            10% off 1 Matcha Milk Tea
+                        </span>
+                    </div>
+                    <PrimaryButton size="large">Redeem Now</PrimaryButton>
+                </div>
+                <p className={styles.note}>
+                    This promotion may only redeemed once. This promotion will
+                    become invalid once the merchant applies the discount to
+                    your purchase.
+                </p>
+            </section>
+            <div className={styles.logoContainer}>
+                <img src={logo} className={styles.logo} />
             </div>
-            <p className={styles.note}>
-                This promotion maye only redeemed once. This promotion will
-                become invalid once the merchant applies the discount to your
-                purchase.
-            </p>
-        </section>
+        </div>
     );
 };
 
